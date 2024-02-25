8e8db4e4d09e478e062153b0c0eaa989875a09db
Carlos0295 <145521914+Carlos0295@users.noreply.github.com>
2024-02-24 22:08:12 -0600

    cambio de materia por asignatura

--- AsignaturaInscritas.java
+++ AsignaturaInscritas.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package asignaturainscritas;
-import java.util.HashMap;
+import java.util.HashMap; 
 //import java.util.Scanner;
 import java.util.Map;
 import javax.swing.JOptionPane; //generar cuadro de dialogo para el usuario
@@ -19,7 +19,7 @@ public class AsignaturaInscritas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Map<String, String> materias = new HashMap<>();
+        Map<String, String> Asignatura = new HashMap<>();
 
         while (true) {
             String inputMateria = JOptionPane.showInputDialog(
@@ -34,42 +34,42 @@ public class AsignaturaInscritas {
 
             switch (opcion) {
                  case 1:
-                    String nombreMateria = JOptionPane.showInputDialog("Ingrese el nombre de la Asignatura: ");
-                    String codigoMateria = JOptionPane.showInputDialog("Ingrese el código de la Asignatura: ");
-                    if (nombreMateria.isEmpty() || codigoMateria.isEmpty()) {
+                    String nombreAsignatura = JOptionPane.showInputDialog("Ingrese el nombre de la Asignatura: ");
+                    String codigoAsignatura = JOptionPane.showInputDialog("Ingrese el código de la Asignatura: ");
+                    if (nombreAsignatura.isEmpty() || codigoAsignatura.isEmpty()) {
                         JOptionPane.showMessageDialog(null, "Error: Debe proporcionar tanto el nombre como el código de la Asignatura.");
                     } else {
-                        materias.put(codigoMateria, nombreMateria);
+                        Asignatura.put(codigoAsignatura, nombreAsignatura);
                         JOptionPane.showMessageDialog(null, "¡Asignaturas agregada con éxito!");
                     }
                     break;
                     
                 case 2:
-                    String materiaEliminar = JOptionPane.showInputDialog("Ingrese el código de la Asignaturas a eliminar: ");
-                    if (materias.containsKey(materiaEliminar)) {
+                    String AsignaturaEliminar = JOptionPane.showInputDialog("Ingrese el código de la Asignaturas a eliminar: ");
+                    if (Asignatura.containsKey(AsignaturaEliminar)) {
                         int confirmacion = JOptionPane.showConfirmDialog(null,"¿Seguro quiere eliminar esta asignatura?", "Advertencia", JOptionPane.WARNING_MESSAGE);
                         if (confirmacion == JOptionPane.YES_OPTION) {
-                            materias.remove(materiaEliminar);
-                            JOptionPane.showMessageDialog(null, "¡Asignaturas eliminada con éxito!");
+                            Asignatura.remove(AsignaturaEliminar);
+                            JOptionPane.showMessageDialog(null, "¡Asignatura eliminada con éxito!");
                         }
                     } else {
-                        JOptionPane.showMessageDialog(null, "La Asignaturas no fue encontrada.", "Error", JOptionPane.ERROR_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "La Asignatura no fue encontrada.", "Error", JOptionPane.ERROR_MESSAGE);
                     }
                     break;
                 case 3:
-                    String materiaBuscar = JOptionPane.showInputDialog("Ingrese el nombre de la Asignaturas a buscar: ");
-                    if (materias.containsKey(materiaBuscar)) {
-                        JOptionPane.showMessageDialog(null, "Descripción de la Asignaturas: " + materias.get(materiaBuscar));
+                    String AsignaturaBuscar = JOptionPane.showInputDialog("Ingrese el nombre de la Asignatura a buscar: ");
+                    if (Asignatura.containsKey(AsignaturaBuscar)) {
+                        JOptionPane.showMessageDialog(null, "Descripción de la Asignatura: " + Asignatura.get(AsignaturaBuscar));
                     } else {
                         JOptionPane.showMessageDialog(null, "Error: La Asignaturas no fue encontrada.", "Error", JOptionPane.ERROR_MESSAGE);
                     }
                     break;
                 case 4:
-                    if (materias.isEmpty()) {
+                    if (Asignatura.isEmpty()) {
                         JOptionPane.showMessageDialog(null, "No hay Asignaturas inscritas.");
                     } else {
                         StringBuilder sb = new StringBuilder("Asignaturas Inscritas:\n");
-                        for (Map.Entry<String, String> entry : materias.entrySet()) {
+                        for (Map.Entry<String, String> entry : Asignatura.entrySet()) {
                             sb.append("Código: ").append(entry.getKey()).append(", Asignaturas: ").append(entry.getValue()).append("\n");
                         }
                         JOptionPane.showMessageDialog(null, sb.toString());
